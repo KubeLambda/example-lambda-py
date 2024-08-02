93cdc053ad9b6aefd3420f53176450b7f052b5c6
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2024-08-02 19:53:19 +0300

    refactor: rename

--- .projenrc.py
+++ .projenrc.py
@@ -4,7 +4,7 @@ project = PythonProject(
     author_email="bfrankovskyi@gmail.com",
     author_name="Bogdan Frankovskyi",
     module_name="example_lambda",
-    name="example_lambda",
+    name="example-lambda-py",
     version="0.1.0",
 )
 
